a2c0f53eee983a880ec06b036d99193e1a2644e2
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-04-10 16:41:10 +0430

    Create user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    has_many :entities
+    has_many :groups
+  
+    validates :firstname, presence: { message: "Firstname can't be blank!" }, length: { minimum: 3, maximum: 250 }
+    validates :lastname, presence: { message: "Lastname can't be blank!" }, length: { minimum: 3, maximum: 250 }
+  end
 end
