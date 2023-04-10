b173ba74dd59daa79cc8d35b412f726233e0b424
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-04-10 16:42:38 +0430

    Finish all the models and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'groups/index'
+  get 'groups/new'
+  get 'groups/create'
+  get 'entities/index'
+  get 'entities/new'
+  get 'entities/create'
   get 'users/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_10_112700) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_10_114302) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "entities", force: :cascade do |t|
+    t.integer "author_id"
+    t.string "name", null: false
+    t.integer "amount", null: false
+    t.datetime "create_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "icon"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "firstname"
@@ -21,4 +39,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_10_112700) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "groups", "users"
 end
