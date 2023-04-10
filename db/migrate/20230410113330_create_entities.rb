d8d9c0f186b92454a1e80c4fa0961ba6ed9b4d76
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-04-10 16:41:46 +0430

    Add associatation relation into groups table

---
+++ db/migrate/20230410113330_create_entities.rb
@@ -0,0 +1,14 @@
+class CreateEntities < ActiveRecord::Migration[7.0]
+  def change
+    create_table :entities do |t|
+      t.integer :author_id
+      t.string :name, null: false
+      t.integer :amount, null: false
+      t.datetime :create_at
+      # t.references :user, null: false, foreign_key: true
+      # t.references :group, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
