716eb08e1caeb55be9eeb646761d3dd97ed6fb16
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-04-11 22:31:50 +0430

    Change the migration

--- db/migrate/20230410113330_create_entities.rb
+++ db/migrate/20230410113330_create_entities.rb
@@ -1,13 +1,12 @@
 class CreateEntities < ActiveRecord::Migration[7.0]
   def change
     create_table :entities do |t|
-      t.integer :author_id
+      # t.integer :group_id
       t.string :name, null: false
       t.integer :amount, null: false
-      t.datetime :create_at
-      # t.references :user, null: false, foreign_key: true
+      # t.datetime :create_at
+      t.references :user, null: false, foreign_key: true
       # t.references :group, null: false, foreign_key: true
-
       t.timestamps
     end
   end
