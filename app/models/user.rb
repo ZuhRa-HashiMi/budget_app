a46be0485fddbd70babe4ea4ec57ee36da9b8a5c
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-04-11 22:30:25 +0430

    Implement the relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
     has_many :entities
     has_many :groups
+    has_one_attached :avatar
   
     # validates :name, presence: { message: "Firstname can't be blank!" }, length: { minimum: 3, maximum: 250 }
 end
