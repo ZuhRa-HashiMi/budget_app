3c80b8011a002f6f7ab8036e3949bbc2c04ee81a
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-04-11 13:35:06 +0430

    Change the database

--- db/schema.rb
+++ db/schema.rb
@@ -34,8 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_10_182912) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "firstname"
-    t.string "lastname"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
