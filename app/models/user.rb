40b016f557a9d47fa555f05e7ee3fce23815d688
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-04-11 13:33:41 +0430

    Change the column of user table

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,5 @@ class User < ApplicationRecord
     has_many :entities
     has_many :groups
   
-    validates :firstname, presence: { message: "Firstname can't be blank!" }, length: { minimum: 3, maximum: 250 }
-    validates :lastname, presence: { message: "Lastname can't be blank!" }, length: { minimum: 3, maximum: 250 }
+    # validates :name, presence: { message: "Firstname can't be blank!" }, length: { minimum: 3, maximum: 250 }
 end
