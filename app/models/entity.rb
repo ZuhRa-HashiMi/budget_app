a59bcf943ef632eae86957661f3979c5bd4cafa2
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-04-11 22:30:14 +0430

    Implement the relation

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,6 +1,7 @@
 class Entity < ApplicationRecord
     belongs_to :user
-    has_and_belongs_to_many :group
+    # belongs_to :group
+    has_and_belongs_to_many :groups
     validates :name, presence: { message: "Name can't be blank!" }
     validates :amount, presence: { message: 'Amount must be greater than Zero (0)' }
 end
