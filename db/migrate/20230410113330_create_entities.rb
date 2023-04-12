bb309d3c21484c04334e794dda04a33fb863b7c9
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-04-13 01:09:12 +0430

    Migrate the entities

--- db/migrate/20230410113330_create_entities.rb
+++ db/migrate/20230410113330_create_entities.rb
@@ -1,7 +1,7 @@
 class CreateEntities < ActiveRecord::Migration[7.0]
   def change
     create_table :entities do |t|
-      # t.integer :group_id
+      t.integer :group_id
       t.string :name, null: false
       t.integer :amount, null: false
       # t.datetime :create_at
