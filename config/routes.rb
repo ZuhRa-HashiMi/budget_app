9f08589cf15b14effac68dabc22fe4bbf6a64080
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-04-13 01:08:47 +0430

    Add path

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
   resources :users, only: :index do
-    resources :groups, only: [:index, :new, :create, :destroy] do
-      resources :entities, only: [:index, :new, :create, :destroy]
+    resources :groups, only: [:index, :new, :create] do
+      resources :entities, only: [:index, :new, :create]
     end
   end
 end
